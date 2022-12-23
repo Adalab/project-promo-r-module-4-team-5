a974c02246d02f55f9ab08a29544f5c5481ea6cc
Bruna <brunadasilvabz98@gmail.com>
2022-12-23 13:49:54 +0100

    add sqlite

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ const randomId = () => {
   return Math.random();
 };
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const Database = require("better-sqlite3");
 
 // Creamos el servidor
 const server = express();
 //Configurador motor de plantillas ejs
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({ limit: '25mb' }));
+server.use(express.json({ limit: "25mb" }));
+
+// URL nueva base de datos
+const db = new Database("./src/db/database.db", { verbose: console.log });
 
 // Arrancamos el servidor en el puerto 4000
 const serverPort = 4000;
@@ -24,18 +29,18 @@ server.listen(serverPort, () => {
 });
 const savedCards = [];
 // Escribimos los endpoints que queramos
-server.post('/card', (req, res) => {
+server.post("/card", (req, res) => {
   if (
-    req.body.palette === '' ||
-    req.body.name === '' ||
-    req.body.job === '' ||
-    req.body.photo === '' ||
-    req.body.linkedin === '' ||
-    req.body.github === '' ||
-    req.body.email === ''
+    req.body.palette === "" ||
+    req.body.name === "" ||
+    req.body.job === "" ||
+    req.body.photo === "" ||
+    req.body.linkedin === "" ||
+    req.body.github === "" ||
+    req.body.email === ""
   ) {
     const responseError = {
-      error: 'Faltan datos',
+      error: "Faltan datos",
       success: false,
     };
     res.json(responseError);
@@ -52,30 +57,28 @@ server.post('/card', (req, res) => {
     res.json(responseSuccess);
   }
 });
-server.get('/card/:cardId', (req, res) => {
-  console.log(req.params);
-  res.render({});
-  // 'card', {
-  //   palette: req.params.palette,
-  //   name: req.params.name,
-  //   job: req.params.job,
-  //   photo: req.params.photo,
-  //   phone: req.params.phone,
-  //   email: req.params.email,
-  //   linkedin: req.params.linkedin,
-  //   github: req.params.github,
-  // });
+
+server.get("/card/:id", (req, res) => {
+  //pintar la tarjeta
+  const id = req.params.id;
+  //encontrar la info de la tarjeta:buscar una tarjeta con un id en la BBDD
+  const query = db.prepare("SELECT * FROM card WHERE id = ?");
+  const userCard = query.get(id);
+  console.log(userCard);
+  res.render("card", userCard);
 });
+
+//// NO NOS RENDERIZA NADA DE NADA
 
 //FALTA POR AÑADIR ERROR TAMAÑO FOTO
 
-const staticServerPathWeb = './src/public-react';
+const staticServerPathWeb = "./src/public-react";
 server.use(express.static(staticServerPathWeb));
 
 // Endpoint para gestionar los errores 404
-server.get('*', (req, res) => {
+server.get("*", (req, res) => {
   // Relativo a este directorio
-  const notFoundFileRelativePath = '../src/public-react/404-not-found.html';
+  const notFoundFileRelativePath = "../src/public-react/404-not-found.html";
   const notFoundFileAbsolutePath = path.join(
     __dirname,
     notFoundFileRelativePath
