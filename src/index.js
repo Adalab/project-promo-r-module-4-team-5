c72f82ad243be4cfcf8aa4e8442b8992bfcbf929
nayraromero <nayraromero@gmail.com>
2022-12-28 13:32:24 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 // Fichero src/index.js
 
-//random number para el id
-// const randomId = () => {
-//   return Math.random();
-// };
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const Database = require("better-sqlite3");
+const Database = require('better-sqlite3');
 
 // Creamos el servidor
 const server = express();
 //Configurador motor de plantillas ejs
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({ limit: "25mb" }));
+server.use(express.json({ limit: '25mb' }));
 
 // URL nueva base de datos
-const db = new Database("./src/db/database.db", { verbose: console.log });
+const db = new Database('./src/db/database.db', { verbose: console.log });
 
 // Arrancamos el servidor en el puerto 4000
 const serverPort = process.env.PORT || 4000;
@@ -29,30 +25,28 @@ server.listen(serverPort, () => {
 });
 const savedCards = [];
 // Escribimos los endpoints que queramos
-server.post("/card", (req, res) => {
+server.post('/card', (req, res) => {
   if (
-    req.body.palette === "" ||
-    req.body.name === "" ||
-    req.body.job === "" ||
-    req.body.photo === "" ||
-    req.body.linkedin === "" ||
-    req.body.github === "" ||
-    req.body.email === ""
+    req.body.palette === '' ||
+    req.body.name === '' ||
+    req.body.job === '' ||
+    req.body.photo === '' ||
+    req.body.linkedin === '' ||
+    req.body.github === '' ||
+    req.body.email === ''
   ) {
     const responseError = {
-      error: "Faltan datos",
+      error: 'Faltan datos',
       success: false,
     };
     res.json(responseError);
   } else {
     const newCard = {
-      // id: randomId(),
       ...req.body,
     };
-    // savedCards.push(newCard);
     //INSERT
     const insertStn = db.prepare(
-      "INSERT INTO card (palette, name, email, photo, phone, linkedin, github, job) VALUES (?,?,?,?,?,?,?,?)"
+      'INSERT INTO card (palette, name, email, photo, phone, linkedin, github, job) VALUES (?,?,?,?,?,?,?,?)'
     );
     const result = insertStn.run(
       newCard.palette,
@@ -74,20 +68,18 @@ server.post("/card", (req, res) => {
     res.json(responseSuccess);
   }
 });
-server.get("/card/:id", (req, res) => {
+server.get('/card/:id', (req, res) => {
   const id = req.params.id;
-  const query = db.prepare("SELECT * FROM card WHERE id = ?");
+  const query = db.prepare('SELECT * FROM card WHERE id = ?');
   const userCard = query.get(id);
-  res.render("card", userCard);
+  res.render('card', userCard);
   console.log(userCard);
 });
 
-//// NO NOS RENDERIZA NADA DE NADA
-
 //FALTA POR AÑADIR ERROR TAMAÑO FOTO
 
-const staticServerPathWeb = "./src/public-react";
+const staticServerPathWeb = './src/public-react';
 server.use(express.static(staticServerPathWeb));
 
-const staticStylesTemplate = "./src/public-styles";
+const staticStylesTemplate = './src/public-styles';
 server.use(express.static(staticStylesTemplate));
